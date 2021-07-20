7c940fd38756cd7b1d90291c238ec8a0245af284
Bernard Yang <ybernard@gmail.com>
2021-07-20 17:32:35 +0000

    final exercise

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -22,3 +22,20 @@ puts "----------"
 # 2. Ask the user for a store name (store it in a variable)
 # 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
 # 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_interger: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+end
+
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+
+puts new_store.errors.full_messages
